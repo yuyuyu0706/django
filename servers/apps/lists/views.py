131003700dab2b6d75409f97b63cc1caf176b563
yuyuyu <rikki.k2008@gmail.com>
2020-06-30 02:02:01 +0900

    auto commit

--- servers/apps/lists/views.py
+++ servers/apps/lists/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View
 from django.views.generic import ListView
 from lists.models import Member
 
 class NWListView(ListView):
 
     model = Member
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'lists/index.html')
