3cfb7a8729713d02aaf13dcd3635e090bed0928a
yuyuyu <rikki.k2008@gmail.com>
2020-06-30 10:32:01 +0900

    auto commit

--- servers/apps/lists/urls.py
+++ servers/apps/lists/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'lists'
  
 urlpatterns = [
-    # path('', views.NWListView.as_view(), name='index'),
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.NWListView.as_view(), name='index'),
+    # path('', views.IndexView.as_view(), name='index'),
 ]
